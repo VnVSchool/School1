daeba28377cf7a1f3d579b0cb25f6d390be1ddf0
dev <l.luzhnuy@gmail.com>
2023-03-02 23:17:45 +0200

    oop2

--- oop/plants_managment/main.py
+++ oop/plants_managment/main.py
@@ -1,7 +1,7 @@
 from models import Plant, Employee
 
 while True:
-    print("1. Add Plant\n2. Gel all plants\n3. Add Employee\n4. Get all employees")
+    print("1. Add Plant\n2. Gel all plants\n3. Add Employee\n4. Get all employees\n5. Update employee\n6. Update Plant")
     flag = int(input("Choose menu item: "))
     if flag == 1:
         name = input("Plant name: ")
@@ -9,7 +9,7 @@ while True:
         plant = Plant(name, address)
         plant.save()
     elif flag == 2:
-        Plant.get_all_plants()
+        Plant.get_all()
     elif flag == 3:
         name = input("Employee name: ")
         email = input("Employee email: ")
@@ -17,4 +17,11 @@ while True:
         employee = Employee(name, email, plant_id)
         employee.save()
     elif flag == 4:
-        Employee.get_all_employees()
+        Employee.get_all()
+    elif flag == 5:
+        id = int(input("Id which employee you want to update: "))
+        employee = Employee.get_el_by_id(id)
+        employee.update()
+    elif flag == 6:
+        id = int(input("Id which plant you want to update: "))
+        Plant.update(id)
--- oop/plants_managment/models/models.py
+++ oop/plants_managment/models/models.py
@@ -1,12 +1,22 @@
 import json
+from framework.models import Model
 
 
-class Plant:
+class Plant(Model):
     file = "database/plants.json"
+    fields = ["name", "address"]
 
-    def __init__(self, name, address):
+    def __init__(self, name, address, id=None):
         self.name = name
         self.address = address
+        self.id = id
+
+    @classmethod
+    def get_el_by_id(cls, id):
+        data = cls.get_file_data()
+        for el in data:
+            if el["id"] == id:
+                return cls(el["name"], el["address"], id=el["id"])
 
     def generate_dict(self):
         return {
@@ -14,38 +24,16 @@ class Plant:
             "address": self.address
         }
 
-    def save(self):
-        self.get_all_plants()
-        file = open(self.file, "r")
-        plants = json.loads(file.read())
-        file.close()
-        file = open(self.file, "w")
-        plant = self.generate_dict()
-        if len(plants) > 1:
-            plant["id"] = len(plants) + 1
-        else:
-            plant["id"] = 1
-        plants.append(plant)
-        file.write(json.dumps(plants))
-        file.close()
 
-    @classmethod
-    def get_all_plants(cls):
-        file = open(cls.file, "r")
-        plants = json.loads(file.read())
-        file.close()
-        for plant in plants:
-            print(plant["name"])
-            print(plant["address"])
-
-
-class Employee:
+class Employee(Model):
     file = "database/employees.json"
+    fields = ["name", "email", "plant_id"]
 
-    def __init__(self, name, email, plant_id):
+    def __init__(self, name, email, plant_id, id=None):
         self.name = name
         self.email = email
         self.plant_id = plant_id
+        self.id = id
 
     def generate_dict(self):
         return {
@@ -54,26 +42,9 @@ class Employee:
             "plant_id": self.plant_id
         }
 
-    def save(self):
-        file = open(self.file, "r")
-        employees = json.loads(file.read())
-        file.close()
-        file = open(self.file, "w")
-        employee = self.generate_dict()
-        if len(employees) > 1:
-            employee["id"] = len(employees) + 1
-        else:
-            employee["id"] = 1
-        employees.append(employee)
-        file.write(json.dumps(employees))
-        file.close()
-
     @classmethod
-    def get_all_employees(cls):
-        file = open(cls.file, "r")
-        employees = json.loads(file.read())
-        file.close()
-        for employee in employees:
-            print(employee["name"])
-            print(employee["email"])
-            print(employee["plant_id"])
+    def get_el_by_id(cls, id):
+        data = cls.get_file_data()
+        for el in data:
+            if el["id"] == id:
+                return cls(el["name"], el["email"], el["plant_id"], id=el["id"])
