fdbfc3a7d61ce88a44ea63123adc1ba11fc19ffa
Liubomyr.Luzhnyi <l.luzhnuy@gmail.com>
2023-03-30 16:40:11 +0300

    flask2

--- flask-app/routes/main/routes.py
+++ flask-app/routes/main/routes.py
@@ -1,10 +1,18 @@
 from app import app
+from models import Article
 from flask import render_template
 
 
 @app.route("/")
 def main():
-    return render_template("main/index.html")
+    articles = Article.query.all()
+    return render_template("main/index.html", articles=articles)
+
+
+@app.route("/article/<int:id>")
+def article_details(id):
+    article = Article.query.get(id)
+    return render_template("main/article_detail.html", article=article)
 
 
 @app.route("/calc/<int:x>/<int:y>")
