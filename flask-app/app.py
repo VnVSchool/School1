41071131100690e329fa8e59be9a9894fbde569b
Liubomyr.Luzhnyi <l.luzhnuy@gmail.com>
2023-04-05 21:51:17 +0300

    flask3

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,12 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-db = SQLAlchemy()
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@db:3306/vnv_blog"
 
-db.init_app(app)
-
+db = SQLAlchemy(app)
 
 with app.app_context():
     from routes.main import *
